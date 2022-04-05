d6cef2956d628340fd96dc053ffa84044ac0fadf
AnnickRynne <annickrynne@gmail.com>
2022-04-05 18:20:37 +0000

    Define 2 functions to add 5 ships on computer board and to collect player's guesses

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+import random
 # Legend:
-# " " available to guess
-# X is hit on guess board
+# " " empty cell available to guess
+# X for placing ship on the computer board and 'hit' ship on the player board
 # - is miss
 
 # THE BOARDS - 8 x 8 cells 
@@ -9,14 +10,16 @@ COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
 #Player board (displayed): used to display the hit and miss targets after each turn
 PLAYER_BOARD = [[" "] * 8 for x in range(8)]
 
-def print_board():
+# The top 3 lines print the columns heading, the for loop creates the grid
+def print_board(board):
     print("  A B C D E F G H")
-    print("  +-+-+-+-+-+-+-+")
+    print("  -+-+-+-+-+-+-+-+")
     row_number = 1
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+# 
 letters_to_numbers = {
     'A': 0,
     'B': 1,
@@ -28,12 +31,25 @@ letters_to_numbers = {
     'H': 7
 }
 
-def create_ships():
-pass
+# Placing 5 ships at random (using the "random" module) by filling 5 empty single cells with an X
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0, 7), randint(0, 7)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = randint(0, 7), randint(0, 7)
+            board[ship_row][ship_column] = "X"
 
 
-def get_ship_coordinates():
-pass
+def get_ship_location():
+    row = input("Enter the row of the ship (1 to 8): ")
+    while row not in "12345678":
+        print('Not an appropriate choice, please select a valid row')
+        row = input("Enter the row of the ship: ")
+    column = input("Enter the column of the ship (A to H): ").upper()
+    while column not in "ABCDEFGH":
+        print('Not an appropriate choice, please select a valid column')
+        column = input("Enter the column of the ship: ")
+    return int(row) - 1, letters_to_numbers[column]
 
 
 def count_hit_ships():
