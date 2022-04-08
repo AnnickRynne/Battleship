9100d80b4565216e13d041c9c74f34a938a22f81
AnnickRynne <annickrynne@gmail.com>
2022-04-08 10:34:39 +0000

    Add player name input and rules

--- run.py
+++ run.py
@@ -1,40 +1,35 @@
 from random import randint
+
 # Legend:
 # " " empty cell available to guess
 # X for placing ship on the computer board and 'hit' ship on the player board
 # - is miss
 
-# name = input("Please enter your name here: " )
-# print(f"Wecome to the game, {name.capitalize()}!")
-
-# def rules():
-#     c_rules = input("Do you know how to play? y/n: ")
-#     if c_rules.lower() == "n":
-#         print(
-#         "You must destroy 5 ships placed at random by the computer "
-#         "on the grid. Each ship occupies one cell. "
-#         "Enter a number for a row and a letter for the column. "
-#         "You have 10 turns to destroy the computer's "
-#         "hidden fleet...GOOD LUCK!"
-#         )
-#         # print("Legend: X for placing ship and hit battleship, "
-#         # "' ' for available space and '-' for missed shot"
-#         # )
-#     elif c_rules.lower() == "y":
-#         print(f"Ok, let's play then! {name.capitalize()}")
-#     else:
-#         print("That is not a valid option. Please try again")
+name = input("Please enter your name here: ")
+print(f"Wecome to the game, {name.capitalize()}!")
 
 
-# THE BOARDS - 8 x 8 cells
-# Computer board (hidden): used to contain 5 ships with random locations
-COMPUTER_BOARD = [[" "] * 8 for x in range(8)]
-# Player board (displayed):
-# used to display the hit and miss targets after each turn
-PLAYER_BOARD = [[" "] * 8 for x in range(8)]
+def rules():
+    c_rules = input("Do you know how to play? y/n: ").lower()
+    if c_rules == "n":
+        print(
+            "You must destroy 5 ships placed at random by the computer "
+            "on the grid. \nEach ship occupies one cell. \n"
+            "Enter a number for a row and a letter for the column. \n"
+            "You have 10 turns to destroy the computer's "
+            "hidden fleet...\nGOOD LUCK!"
+            )
+        if input("Ready to play? y/n: ").lower() != "y":
+            print("Sorry to see you leave...QUIT")
+            quit()
+    elif c_rules == "y":
+        print("Ok, let's play then!")
+        play_game()
+    else:
+        print("That is not a valid option. Please try again")
+        rules()
 
 
-# The top 3 lines print the columns heading, the for loop creates the grid
 def print_board(board):
     print("  A B C D E F G H")
     print("  +-+-+-+-+-+-+-+")
@@ -43,17 +38,16 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
-# Python can only process numbers
 letters_to_numbers = {
-    'A': 0,
-    'B': 1,
-    'C': 2,
-    'D': 3,
-    'E': 4,
-    'F': 5,
-    'G': 6,
-    'H': 7
-}
+        'A': 0,
+        'B': 1,
+        'C': 2,
+        'D': 3,
+        'E': 4,
+        'F': 5,
+        'G': 6,
+        'H': 7
+    }
 
 # Placing 5 ships at random by filling 5 empty cells with an X
 def create_ships(board):
@@ -71,7 +65,7 @@ def get_ship_location():
     while row not in "12345678":
         print("Not an appropriate choice, please enter a valid row")
         row = input("Enter the row of the ship (1 to 8): ")
-    column = input("Enter the column of the ship (A to H): ").upper()
+    column = input("Enter the row of the ship (A to H): ").upper()
     while column not in "ABCDEFGH":
         print("Not an appropriate choice, please select a valid column")
         column = input("Enter the column of the ship (A to H): ").upper()
@@ -120,21 +114,9 @@ def play_game():
             break
 
 
-play_game()
-
-# def stop_playing():
-#     play_game = True
-#     while play_game:
-#         continue_playing = input("Would you like to continue? y/n: ")
-#         if continue_playing.lower() == "y":
-#             print("You said Yes, continue")
-#         elif continue_playing.lower() == "n":
-#             print("Stopping the game...")
-#             play_game = False
-#         else:
-#             print("That is not a valid option. Please try again")
+def main():
+    rules()
+    play_game()
 
 
-# def main():
-#     stop_playing()
-# main()
+main()
